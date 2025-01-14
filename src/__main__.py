c79f32431f8ebdec475019289273e61e1cd5b2f1
Andrew Nolan <Andrew_Nolan@artexrisk.com>
2025-01-14 18:15:00 +0000

    better shutdown

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,4 @@
-import signal
+import keyboard
 
 from .browser import EdgeLauncherThread
 from .export import FitbitDataDaemon
@@ -6,23 +6,26 @@ from .dash_app import app
 from .utils import kill_python_processes
 
 def main():
-    def signal_handler(sig, frame):
-        print("\nYou pressed Ctrl+C! Shutting down gracefully...")
-        try:
-            # daemon.stop()  # Stop the FitbitDataDaemon
-            1
-        except Exception as e:
-            print(f"Error while shutting down the server: {e}")
-        finally:
-            print("Exiting...")
-            kill_python_processes()
+    def CtrlShiftC_handler():
+        print("\nYou pressed Ctrl+shift+C! Shutting down gracefully...")
+        # try:
+        #     # daemon.stop()  # Stop the FitbitDataDaemon
+        #     1
+        # except Exception as e:
+        #     print(f"Error while shutting down the server: {e}")
+        # finally:
+            # print("Exiting...")
+            # kill_python_processes()
+        print("Exiting...")
+        kill_python_processes()        
         exit(0)
 
-    # Attach the signal handler for graceful shutdown
-    signal.signal(signal.SIGINT, signal_handler)
+
+    # Attach the signal handler for forced shutdown
+    keyboard.add_hotkey("ctrl+shift+c", CtrlShiftC_handler)
 
     print("Starting FitbitDataDaemon...")
-    print("Press Ctrl+C to stop the program.")
+    print("Press Ctrl+Shift+C to stop the program.")
 
     daemon = FitbitDataDaemon()
     browser = EdgeLauncherThread()
