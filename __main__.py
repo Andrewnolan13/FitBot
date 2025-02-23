24bb0514bc18d0735d79e19c4f1750b86b42dd33
andrew nolan <andrewnolan871@gmail.com>
2025-02-23 18:34:36 +0000

    make table on run of __main__

--- __main__.py
+++ __main__.py
@@ -1,4 +1,25 @@
 from src import main
+import sqlite3
+from src.constants import DB_PATH
+
+conn = sqlite3.connect(DB_PATH)
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS fitbit (
+        scrapeTime TEXT,
+        dateTime TEXT,
+        steps INTEGER,
+        weight REAL,
+        calories INTEGER,
+        carbs INTEGER,
+        fat INTEGER,
+        protein INTEGER,
+        sodium INTEGER
+    );
+""")
+conn.commit()
+print("Table created successfully.")
+conn.close()
 
 if __name__ == '__main__':
     main()
