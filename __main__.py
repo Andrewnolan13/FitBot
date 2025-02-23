a2f29d1fbb34386a0dc3a856ecfd81590b48ddfa
andrew nolan <andrewnolan871@gmail.com>
2025-02-23 18:39:05 +0000

    jfc

--- __main__.py
+++ __main__.py
@@ -1,25 +1,26 @@
-from src import main
 import sqlite3
 from src.constants import DB_PATH
 
-if __name__ == '__main__':
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS fitbit (
-            scrapeTime TEXT,
-            dateTime TEXT,
-            steps INTEGER,
-            weight REAL,
-            calories INTEGER,
-            carbs INTEGER,
-            fat INTEGER,
-            protein INTEGER,
-            sodium INTEGER
-        );
-    """)
-    conn.commit()
-    print("Table created successfully.")
-    conn.close()
+conn = sqlite3.connect(DB_PATH)
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS fitbit (
+        scrapeTime TEXT,
+        dateTime TEXT,
+        steps INTEGER,
+        weight REAL,
+        calories INTEGER,
+        carbs INTEGER,
+        fat INTEGER,
+        protein INTEGER,
+        sodium INTEGER
+    );
+""")
+conn.commit()
+print("Table created successfully.")
+conn.close()
 
+from src import main
+
+if __name__ == '__main__':
     main()
