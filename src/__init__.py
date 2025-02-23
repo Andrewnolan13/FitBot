982eaa39c20594f9a7eb8cdaf5272db015e54757
andrew nolan <andrewnolan871@gmail.com>
2025-02-23 18:40:56 +0000

    jfc

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,25 @@
-from .export import FitbitDataDaemon
-from .__main__ import main
 import sqlite3
 from .constants import DB_PATH
+
+conn = sqlite3.connect(DB_PATH)
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS fitbit (
+        scrapeTime TEXT,
+        dateTime TEXT,
+        steps INTEGER,
+        weight REAL,
+        calories INTEGER,
+        carbs INTEGER,
+        fat INTEGER,
+        protein INTEGER,
+        sodium INTEGER
+    );
+""")
+conn.commit()
+print("Table created successfully.")
+conn.close()
+
+
+from .export import FitbitDataDaemon
+from .__main__ import main
